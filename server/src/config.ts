7678d68be2d75ebc27cb193f3e775538a53bdc01
Bazhan Nagnybida <bazhantt@gmail.com>
2023-02-26 01:53:02 -0500

    testing

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,15 +1,18 @@
+// export const mainDBName = "mainTest";
+// export const ordersDBName = "ordersTest";
+// export const sessionsDBName = "sessionsTest";
+// export const dishesDBName = "dishesTest";
 
 
-
-
-export const mainDBName = "mainTest";
-export const ordersDBName = "ordersTest";
-export const sessionsDBName = "sessionsTest";
-export const dishesDBName = "dishesTest";
-
-
-// export const mainDBName = "main";
-// export const ordersDBName = "orders";
-// export const sessionsDBName = "sessions";
-// export const dishesDBName = "dishes";
-
+export const mainDBName: string = await new Promise(res => setTimeout(() => {
+    res(process.env.MAIN_DB_NAME as string);
+}, 10));
+export const ordersDBName: string = await new Promise(res => setTimeout(() => {
+    res(process.env.ORDERS_DB_NAME as string);
+}, 10));
+export const sessionsDBName: string = await new Promise(res => setTimeout(() => {
+    res(process.env.SESSIONS_DB_NAME as string);
+}, 10));
+export const dishesDBName: string = await new Promise(res => setTimeout(() => {
+    res(process.env.DISHES_DB_NAME as string);
+}, 10));
